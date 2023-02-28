f322f347c79e2cf43ff576909fb67deaf52db13a
irriz-jayy <jayfelix789@gmail.com>
2023-02-28 12:06:47 +0300

    Added header and card for blogs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import "../App.css";
 
 const Header = () => {
-  return <div>hello world</div>;
+  return (
+    <nav className="navbar navbar-light bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          <img
+            src="https://raw.githubusercontent.com/irriz-jayy/portfolio_02/main/public/favicon.ico"
+            alt=""
+            width="30"
+            height="24"
+          />
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
