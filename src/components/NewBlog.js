6ca47c6c82a8c5236f57d7a189c7e195dacd0f07
irriz-jayy <jayfelix789@gmail.com>
2023-02-28 15:47:12 +0300

    Added form for input

--- src/components/NewBlog.js
+++ src/components/NewBlog.js
@@ -1,7 +1,47 @@
+import "../App.css";
+
 const NewBlog = () => {
   return (
-    <div className="test">
-      <h2>hello world</h2>
+    <div className="item-container">
+      <form>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Title"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">
+            Content
+          </label>
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">
+            Author
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Author"
+          />
+        </div>
+        <div class="col-auto">
+          <button type="submit" class="btn btn-primary mb-3">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
