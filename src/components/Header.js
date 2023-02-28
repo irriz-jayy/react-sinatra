cae5a9364c35c0d2cde9008700a9390c79f40bcf
irriz-jayy <jayfelix789@gmail.com>
2023-02-28 15:06:29 +0300

    Changed and updated roots

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <Link className="navbar-brand" activeClassName="active" exact to="/">
           <img
             src="https://raw.githubusercontent.com/irriz-jayy/portfolio_02/main/public/favicon.ico"
             alt=""
@@ -13,8 +13,8 @@ const Header = () => {
             height="24"
             className="d-inline-block align-text-top"
           />
-          Jay's Blog
-        </a>
+          Jayy's Blog
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -29,17 +29,12 @@ const Header = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <NavLink
-                className="nav-link"
-                activeClassName="active"
-                exact
-                to="/"
-              >
+              <NavLink className="nav-link" activeclassname="active" to="/">
                 Home
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" activeClassName="active" to="/blog">
+              <NavLink className="nav-link" activeclassname="active" to="/blog">
                 New Blog
               </NavLink>
             </li>
