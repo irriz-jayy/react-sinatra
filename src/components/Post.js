108f341fa210fd499a2d46fa0d488df85765f6c3
irriz-jayy <jayfelix789@gmail.com>
2023-03-02 20:17:51 +0300

    Added blog details

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,41 +1,98 @@
-import "../App.css";
+// import "../App.css";
+// import { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+
+// const Post = () => {
+//   const [blogs, setBlogs] = useState([]);
+
+//   useEffect(() => {
+//     fetch("http://localhost:9292/blogs")
+//       .then((response) => response.json())
+//       .then((blogs) => setBlogs(blogs));
+//   }, []); // <-- add an empty dependency array here
+
+//   const handleDelete = async (id) => {
+//     console.log("clicked", id);
+//     try {
+//       const response = await fetch(`http://localhost:9292/blogs/${id}`, {
+//         method: "DELETE",
+//       });
+//       if (response.ok) {
+//         alert("Blog deleted successfully");
+//         setBlogs(blogs.filter((blog) => blog.id !== id));
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   return (
+//     <div className="card">
+//       {blogs.map((blog, id) => (
+//         <div className="card-body" key={id}>
+//           <h5 className="card-title">{blog.title}</h5>
+//           <p className="card-text">{blog.content}</p>
+//           <Link to={`/view/${blog.id}`} className="btn btn-primary">
+//             View blog
+//           </Link>
+//           {"  "}
+//           <a
+//             href="#"
+//             className="btn btn-danger"
+//             onClick={() => handleDelete(blog.id)}
+//           >
+//             Delete
+//           </a>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Post;
+
+// Post.js
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Post = () => {
   const [blogs, setBlogs] = useState([]);
+  const [selectedBlog, setSelectedBlog] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:9292/blogs")
       .then((response) => response.json())
       .then((blogs) => setBlogs(blogs));
-  }, []); // <-- add an empty dependency array here
+  }, []);
 
-  const handleDelete = async (id) => {
-    console.log("clicked", id);
-    try {
-      const response = await fetch(`http://localhost:9292/blogs/${id}`, {
-        method: "DELETE",
+  const handleDelete = (id) => {
+    fetch(`http://localhost:9292/blogs/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          setBlogs(blogs.filter((blog) => blog.id !== id));
+        } else {
+          throw new Error("Something went wrong while deleting the blog.");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
       });
-      if (response.ok) {
-        alert("Blog deleted successfully");
-        setBlogs(blogs.filter((blog) => blog.id !== id));
-      }
-    } catch (error) {
-      console.error(error);
-    }
   };
 
   return (
     <div className="card">
-      {blogs.map((blog, id) => (
-        <div className="card-body" key={id}>
+      {blogs.map((blog) => (
+        <div className="card-body" key={blog.id}>
           <h5 className="card-title">{blog.title}</h5>
           <p className="card-text">{blog.content}</p>
+          <p className="card-text">
+            <small className="text-muted">Author ID: {blog.author_id}</small>
+          </p>
           <Link to={`/view/${blog.id}`} className="btn btn-primary">
             View blog
-          </Link>
-          {"  "}
+          </Link>{" "}
           <a
             href="#"
             className="btn btn-danger"
--- src/components/View.js
+++ src/components/View.js
@@ -1,13 +1,30 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const View = () => {
   const { id } = useParams();
+  const [blog, setBlog] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/blogs/${id}`)
+      .then((response) => response.json())
+      .then((data) => setBlog(data))
+      .catch((error) => console.log(error));
+  }, [id]);
 
   return (
     <div>
       <h1>Blog Details</h1>
-      <p>Blog ID: {id}</p>
-      {/* Display other blog details here */}
+      {blog ? (
+        <>
+          <p>Blog ID: {blog.id}</p>
+          <p>Title: {blog.title}</p>
+          <p>Content: {blog.content}</p>
+          <p>Author ID: {blog.author_id}</p>
+        </>
+      ) : (
+        <p>Loading blog details...</p>
+      )}
     </div>
   );
 };
